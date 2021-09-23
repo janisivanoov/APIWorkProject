f2128df67e26c9db34eb9866d5b20f78dfdfa90c
Janis <janisivanov@icloud.com>
2021-09-23 17:54:32 +0300

    Add project files.

---
+++ mysqltest/Controllers/ApiControllerBase.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using mysqltest.Mapping.DTO;
+using mysqltest.Models;
+using mysqltest.Paging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mysqltest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+        protected readonly IMapper _mapper;
+        protected readonly ClubsContext _context;
+
+        public ApiControllerBase(ClubsContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public List<ClubDTO> Paginate<TDto>(IQueryable query, QueryClubParameters QueryClubParameters)
+        {
+            List<ClubDTO> clubs;
+
+            if (QueryClubParameters.Name == "ALL")
+            {
+                clubs = query.ProjectTo<ClubDTO>(_mapper.ConfigurationProvider)
+                            .Skip((QueryClubParameters.PageNumber - 1) * QueryClubParameters.PageSize) //Skippin adding process PageNumber and PageSize
+                            .Take(QueryClubParameters.PageSize) //Using PageSize
+                            .ToList(); //Sending to List
+            }
+            else
+            {
+                clubs = query.ProjectTo<ClubDTO>(_mapper.ConfigurationProvider)
+                          .Where(c => c.Name == QueryClubParameters.Name)
+                          .Skip((QueryClubParameters.PageNumber - 1) * QueryClubParameters.PageSize) //Skippin adding process PageNumber and PageSize
+                          .Take(QueryClubParameters.PageSize) //Using PageSize
+                          .ToList(); //Sending to List
+            }
+
+            return clubs;
+        }
+
+        public List<StudentDTO> Paginate<TDTO>(IQueryable<TDTO> query, QueryStudentParameters QueryStudentParameters)
+        {
+            List<StudentDTO> students;
+
+            if (QueryStudentParameters.Name == "ALL")
+            {
+                return query.ProjectTo<StudentDTO>(_mapper.ConfigurationProvider)
+                                .Skip((QueryStudentParameters.PageNumber - 1) * QueryStudentParameters.PageSize) //Skipping adding process PageNumber and PageSize
+                                .Take(QueryStudentParameters.PageSize) //Using PageSize
+                                .ToList(); //Sending to List
+            }
+            else
+            {
+                return query.ProjectTo<StudentDTO>(_mapper.ConfigurationProvider)
+                             .Skip((QueryStudentParameters.PageNumber - 1) * QueryStudentParameters.PageSize) //Skipping adding process PageNumber and PageSize
+                             .Take(QueryStudentParameters.PageSize) //Using PageSize
+                             .ToList(); //Sending to List}
+            }
+            return students;
+        }
+    }
+}
---
+++ mysqltest/Paging/QueryClubParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace mysqltest.Paging
+{
+    public class QueryClubParameters
+    {
+        private const int maxPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+
+        [Range(1, maxPageSize)]
+        public int PageSize { get; set; }
+
+        public string Name { get; set; }
+    }
+}
